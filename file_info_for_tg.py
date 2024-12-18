1720813ca48cb31de61ee84e8b0dc079f3b68a0d
MrGrose <grachev.ro@gmail.com>
2024-12-18 21:04:40 +0300

    Fixed code

--- file_info_for_tg.py
+++ file_info_for_tg.py
@@ -2,14 +2,21 @@ from pathlib import Path
 from random import choice
 
 
-def get_file_info() -> dict[str]:
+def get_file_info(number_photo: str) -> dict[str]:
+
     base_dir = Path(__file__).parent / 'images'
     photos_name = [photo.name for photo in base_dir.iterdir()]
 
-    photo_name = choice(photos_name)
-    file_path = f'images/{photo_name}'
+    if not number_photo:
+        photo_name = choice(photos_name)
+        file_path = f'images/{photo_name}'
+        return {
+            'file_name': photo_name,
+            'file_path': file_path
+            }
 
-    return {
-        'file_name': photo_name,
-        'file_path': file_path
-        }
+    else:
+        return {
+            'file_name': photos_name[int(number_photo)],
+            'file_path': f'images/{photos_name[int(number_photo)]}'
+            }
