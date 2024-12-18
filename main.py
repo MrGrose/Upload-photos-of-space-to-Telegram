0813e715be573ce035612d788dae193ead9733cd
MrGrose <grachev.ro@gmail.com>
2024-12-18 12:56:39 +0300

    Added main to run through the console

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 from pathlib import Path
 
+from arg_parser import create_parser
 from dotenv import load_dotenv
 from fetch_spacex_images import fetch_spacex_last_launch
-from load_nasa_image import get_nasa_image
 from load_epic_image import get_epic_image
+from load_nasa_image import get_nasa_image
 
 
 def main():
@@ -12,8 +13,14 @@ def main():
     token = os.getenv('NASA_API_KEY')
     base_dir = Path(__file__).parent / 'images'
     base_dir.mkdir(exist_ok=True)
-    fetch_spacex_last_launch(base_dir)
-    get_nasa_image(base_dir, token)
+    parser = create_parser()
+    parsed_args = parser.parse_args()
+    if parsed_args.id is not None and parsed_args.id.isdigit():
+        parsed_args.quantity = int(parsed_args.id)
+        parsed_args.id = None
+
+    fetch_spacex_last_launch(base_dir, parsed_args.id)
+    get_nasa_image(base_dir, token, parsed_args.quantity)
     get_epic_image(base_dir, token)
 
 
