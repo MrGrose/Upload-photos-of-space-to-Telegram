1ca998fca4040cb595a49a00e8a8f6fd2e418df8
MrGrose <grachev.ro@gmail.com>
2024-12-19 19:03:33 +0300

    Fixed code
    - Have completed the description
    - Filled it out in English
    - Add param -n 'Sending a photo by number'

--- arg_parser.py
+++ arg_parser.py
@@ -2,18 +2,22 @@ import argparse
 
 
 def create_parser() -> 'argparse.ArgumentParser':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='''
+        Script for uploading and sending photos from various space APIs
+        ''')
     parser.add_argument(
-        'id',
-        nargs='?',
-        help='Загрузка по id, если он передан',
-        type=str
+        '-id',
+        help='Uploading by id',
     )
     parser.add_argument(
-        'quantity',
-        nargs='?',
+        '-q',
         default=10,
-        help='Количество страниц для загрузки',
-        type=int
+        help='Number of pages to download',
+    )
+    parser.add_argument(
+        '-n',
+        help='Sending a photo by number',
     )
     return parser
